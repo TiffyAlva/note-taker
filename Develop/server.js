08ad5aa43c445b28fffc0375c36fa1d678ade1db
TiffyAlva <TiffanyAlvarez54@gmail.com>
2022-10-22 18:05:22 -0400

    api router

---
+++ Develop/server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
+const app = express ()
+const db = require("./db/db.json")
+app.use(express.static("public"))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname,"./public/notes.html"))
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+})
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname,"./db/db.json"))
+})
+
+app.post("/api/notes", (req, res) => {
+const data = req.body 
+console.log(data)
+db.push(data)
+fs.writeFile("./db/db.json", JSON.stringify(db), () => {
+    res.sendFile(path.join(__dirname,"./db/db.json"))
+})
+    
+})
+
+app.listen(3001, () => {
+    console.log("server is running")
+})
